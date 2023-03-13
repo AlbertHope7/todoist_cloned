6cc3aa5779e3475e9ad0287e549692ed6b04d8b6
AlbertHope7 <byiringiroalbert20@gmail.com>
2023-03-13 08:33:16 +0200

    add todos models

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    task = models.CharField(max_length=250)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    due_until = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.task
